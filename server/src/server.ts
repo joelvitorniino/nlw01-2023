52cab260208763a6e0f4f0d5223e2e7138462542
joelvitorniino <joelprofessor13@gmail.com>
2023-01-17 13:58:25 -0300

    add cors in api fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { PrismaClient } from '@prisma/client';
-
+import cors from '@fastify/cors';
 
 const app = fastify();
 const prisma = new PrismaClient();
+
+app.register(cors)
 
 app.get('/', async () => {
     const habits = await prisma.habit.findMany();
