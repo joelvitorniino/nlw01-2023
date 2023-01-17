9582d8600128dbfa61bf6e5c6d7c55f8e4d49a9e
joelvitorniino <joelprofessor13@gmail.com>
2023-01-17 13:50:40 -0300

    get all habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-    return "Hello, World!";
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany();
+
+    return habits;
 });
 
 app.listen({
