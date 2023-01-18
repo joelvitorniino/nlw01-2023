a2248ddb6c2f88da8cf79b0b2e109d2aa28b4e57
joelvitorniino <joelprofessor13@gmail.com>
2023-01-18 13:01:23 -0300

    new local

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany();
+
+    return habits;
+});
+
+app.listen({
+    port: 3000
+})
+    .then(() => {
+        console.log("Server running on http://localhost:3000/");
+    });
