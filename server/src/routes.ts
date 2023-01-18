d34634e7a280fd5ff37c31b5927c48751c41dfc4
joelvitorniino <joelprofessor13@gmail.com>
2023-01-18 15:19:42 -0300

    returning on routes day only dayHabit id in completedHabits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -38,7 +38,8 @@ export async function appRoutes(app: FastifyInstance) {
 
         const { date } = getDayParams.parse(request.query);
 
-        const weekDay = dayjs(date).get('day');
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day');
 
         const possibleHabits = await prisma.habit.findMany({
             where: {
@@ -51,10 +52,24 @@ export async function appRoutes(app: FastifyInstance) {
                     }
                 }
             }
-        })
+        });
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        });
+        
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id;
+        });
 
         return {
-            possibleHabits
-        }
+            possibleHabits,
+            completedHabits
+        };
     });
-}
+};
