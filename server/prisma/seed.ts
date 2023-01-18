be6da2d281e2c43850e3f6528a71a1e47da15484
joelvitorniino <joelprofessor13@gmail.com>
2023-01-18 14:22:38 -0300

    seeding database on prisma

---
+++ server/prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.habit.create({
+        data: {
+            title: 'Beber 2L de água',
+            created_at: new Date('2023-01-18T:00:00:00.000z')
+        }
+    })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
