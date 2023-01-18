74b320324cad83ea90f7a9ab3c0b44c3c185c4b3
joelvitorniino <joelprofessor13@gmail.com>
2023-01-18 15:50:23 -0300

    component HabitDay

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+// interface HabitDayProps {
+//     completed: number;
+// }
+
+export default function HabitDay() {
+    return (
+        <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
+            <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        </div>
+    )
+}
