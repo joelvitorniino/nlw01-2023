7219c06e98c126ada785117e586d69f2789e51d6
joelvitorniino <joelprofessor13@gmail.com>
2023-01-20 17:41:01 -0300

    specific host on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
 import cors from '@fastify/cors';
 import { appRoutes } from "./routes";
+import { config } from "dotenv";
+
+config();
 
 const app = fastify();
 
@@ -8,6 +11,7 @@ app.register(cors);
 app.register(appRoutes)
 
 app.listen({
+    host: process.env.MY_IP,
     port: 3000
 })
     .then(() => {
