7f57013f571721716c5d59fb020d4662c7d19aeb
joelvitorniino <joelprofessor13@gmail.com>
2023-01-20 18:08:06 -0300

    send form to back end

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,13 +1,15 @@
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import BackButton from "../components/BackButton";
 import CheckBox from "../components/CheckBox";
 import { useState } from "react";
 import { Feather } from '@expo/vector-icons';
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export default function New() {
+    const [title, setTitle] = useState('');
     const [weekDays, setWeekDays] = useState<number[]>([]);
 
     const handleToggleWeekDays = (weekDayIndex: number) => {
@@ -16,6 +18,24 @@ export default function New() {
         } else {
             setWeekDays(prevState => [...prevState, weekDayIndex]);
         };
+    };
+
+    const handleCreateNewHabit = async () => {
+        try {
+            if(!title.trim() || weekDays.length === 0) {
+                Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+            };
+
+            await api.post('/habits', { title, weekDays });
+
+            setTitle('');
+            setWeekDays([]);
+
+            Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+        } catch(e) {
+            console.log(e);
+            Alert.alert('Ops', 'Não foi possível criar o novo hábito');
+        }
     };
 
     return (
@@ -31,7 +51,7 @@ export default function New() {
                     Qual seu comprometimento?
                 </Text>
 
-                <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600" placeholder="Exercícios, dormir bem, etc..." placeholderTextColor={colors.zinc[400]} />
+                <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600" placeholder="Exercícios, dormir bem, etc..." placeholderTextColor={colors.zinc[400]} value={title} onChangeText={setTitle} />
 
                 <Text className="font-semibold mt-4 mb-3 text-white text-base">
                     Qual a recorrência?
@@ -45,7 +65,7 @@ export default function New() {
                     })
                 }
 
-                <TouchableOpacity className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6" activeOpacity={0.7}>
+                <TouchableOpacity className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6" activeOpacity={0.7} onPress={handleCreateNewHabit}>
                     <Feather name="check" size={20} color={colors.white} />
 
                     <Text className="font-semibold text-base text-white ml-2">
