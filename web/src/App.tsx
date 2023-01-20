18cb351509df9fc5cbf20a5571ec1626b69afb4e
joelvitorniino <joelprofessor13@gmail.com>
2023-01-20 16:47:10 -0300

    add locale br in dayjs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import SummaryTable from './components/SummaryTable';
 import './styles/global.css';
 // import Habit from './components/Habit'
+import './lib/dayjs';
 
 function App() {
   return (
