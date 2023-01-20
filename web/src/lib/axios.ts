782191ae2a1ea1e026494c80bea9b37214118b1f
joelvitorniino <joelprofessor13@gmail.com>
2023-01-20 19:17:15 -0300

    add baseUrl in env

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
+import { config } from "dotenv";
+
+config();
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: `http://${process.env.MY_IP}:3000`
 });
