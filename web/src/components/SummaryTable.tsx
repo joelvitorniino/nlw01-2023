aa91987c716a8598f622d4d56f905b382d703872
joelvitorniino <joelprofessor13@gmail.com>
2023-01-20 16:54:23 -0300

    integration with server side

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,42 +1,71 @@
+import { useEffect, useState } from "react";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import HabitDay from "./HabitDay";
+import { api } from "../lib/axios";
+import dayjs from "dayjs";
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
 
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>;
+
 export default function SummaryTable() {
-    return (
-        <div className="w-full flex">
-            <div className="grid grid-rows-7 grid-flow-row gap-3">
-               { weekDays.map((weekDay, i) => {
-                return (
-                    <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-                        {weekDay}
-                    </div>
-                );
-               })}
-            </div>
+  const [summary, setSummary] = useState<Summary>([]);
 
-            <div className="grid grid-rows-7 grid-flow-col gap-3">
-               { summaryDates.map((date, i) => {
-                return (
-                    <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={`${date}-${i}`} />
-                );
-               })}
+  useEffect(() => {
+    api.get("summary").then((response) => setSummary(response.data));
+  }, []);
 
-               { amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-                return (
-                    <div 
-                        key={i} 
-                        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
-                    />
-                );
-               }) }
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date, i) => {
+          const dayInSummmary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day');
+          });
+
+          return (
+            <HabitDay
+              date={date}
+              amount={dayInSummmary?.amount}
+              completed={dayInSummmary?.completed}
+              key={`${date}-${i}`}
+            />
+          );
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
